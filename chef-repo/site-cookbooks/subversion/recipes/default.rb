938230ea1bf1adcc1a1257a95ca1fc15cc5535b6
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-13 14:43:34 -0800

    Create recipe to install subversion #3

---
+++ chef-repo/site-cookbooks/subversion/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: subversion
+# Recipe:: default
+#
+# Copyright 2015, NASEBANAL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "subversion" do
+	action :upgrade
+	version "1.6.11"
+end
