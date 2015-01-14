1bb42eab0711be701940584dab67821a5d4f83e9
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-13 20:16:47 -0800

    Modify recipe to add osdk_admin to rvm group #4

--- chef-repo/site-cookbooks/rvm/recipes/default.rb
+++ chef-repo/site-cookbooks/rvm/recipes/default.rb
@@ -14,6 +14,16 @@ bash 'install rvm' do
     cwd node['rvm']['working_dir']
     code <<-EOH
 gpg2 --keyserver #{node['rvm']['key_url']} --recv-keys #{node['rvm']['recv_keys']}
-curl -sSL #{node['rvm']['site_url']} | bash -s stable
+curl -sSL #{node['rvm']['site_url']} | bash -s stable --version 1.26.9
+source /etc/profile.d/rvm.sh
+rvm install 2.1.3
 EOH
+end
+
+# Add osdk_admin to rvm group
+
+group "rvm" do
+	action :modify
+	members "osdk_admin"
+	append true
 end
