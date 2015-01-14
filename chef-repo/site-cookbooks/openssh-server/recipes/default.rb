e7d544d6e0a5a6d60cd2fadbc3a19b367ce25f51
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-14 01:08:50 -0800

    Create recipe to install and launch sshd #8

---
+++ chef-repo/site-cookbooks/openssh-server/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: openssh-server
+# Recipe:: default
+#
+# Copyright 2015, NASEBANAL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "openssh-server" do
+	action :upgrade
+	version "5.3"
+end
+
+service "sshd" do
+	action :start
+end
