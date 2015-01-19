77318f73b43846ef861de60f2da30f6753800cb8
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-18 23:41:03 -0800

    Modify recipe for rvm install #4

--- chef-repo/site-cookbooks/rvm/attributes/default.rb
+++ chef-repo/site-cookbooks/rvm/attributes/default.rb
@@ -1,4 +1,16 @@
 ## Repository
+
 default['rvm']['site_url'] = 'https://get.rvm.io'
 default['rvm']['key_url'] = 'hkp://keys.gnupg.net'
 default['rvm']['recv_keys'] = '409B6B1796C275462A1703113804BB82D39DC0E3'
+
+## Directory
+
+default['rvm']['working_dir'] = ::File.join(Chef::Config[:file_cache_path], 'rvm')
+
+
+## File
+
+default['rvm']['install_sh'] = 'install_ruby.sh'
+default['rvm']['install_sh_path'] = ::File.join(node['rvm']['working_dir'], node['rvm']['install_sh'])
+
--- chef-repo/site-cookbooks/rvm/recipes/default.rb
+++ chef-repo/site-cookbooks/rvm/recipes/default.rb
@@ -7,16 +7,28 @@
 # All rights reserved - Do Not Redistribute
 #
 
+## Create work directory
+
+directory node['rvm']['working_dir'] do
+	action :create
+end
+
+
+## Copy install_ruby.sh
+
+template node['rvm']['install_sh_path'] do
+	source node['rvm']['install_sh']
+	mode "0755"
+end
+
+
 ## Install RVM
 
 bash 'install rvm' do
     action :run
     cwd node['rvm']['working_dir']
     code <<-EOH
-gpg2 --keyserver #{node['rvm']['key_url']} --recv-keys #{node['rvm']['recv_keys']}
-curl -sSL #{node['rvm']['site_url']} | bash -s stable --version 1.26.9
-source /etc/profile.d/rvm.sh
-rvm install 2.1.3
+#{node['rvm']['install_sh_path']} #{node['rvm']['key_url']} #{node['rvm']['recv_keys']} #{node['rvm']['site_url']} 1.26.9 2.1.3 #{node['rvm']['working_dir']}
 EOH
 end
 
