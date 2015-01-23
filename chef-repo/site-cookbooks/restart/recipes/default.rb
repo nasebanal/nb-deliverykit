bbe070904cc0c8313a31d6dc327002a40c8126f9
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-23 14:22:36 -0800

    Add start, restart node

---
+++ chef-repo/site-cookbooks/restart/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: start
+# Recipe:: default
+#
+# Copyright 2015, NASEBANAL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+## Start sshd
+
+service "sshd" do
+	action :restart
+end
+
+
+## Start httpd
+
+service "httpd" do
+	action :restart
+end
+
+
+## Start mysql-server
+
+service "mysqld" do
+	action :restart
+end
+
+
+## Start subversion
+
+service "svnserve" do
+	action :restart
+end
+
+
+## Start jenkins
+
+service "jenkins" do
+	action :restart
+end
---
+++ chef-repo/site-cookbooks/start/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: start
+# Recipe:: default
+#
+# Copyright 2015, NASEBANAL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+## Start sshd
+
+service "sshd" do
+	action :start
+end
+
+
+## Start httpd
+
+service "httpd" do
+	action :start
+end
+
+
+## Start mysql-server
+
+service "mysqld" do
+	action :start
+end
+
+
+## Start subversion
+
+service "svnserve" do
+	action :start
+end
+
+
+## Start jenkins
+
+service "jenkins" do
+	action :start
+end
