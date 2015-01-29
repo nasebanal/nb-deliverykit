8dceb3c474537ce48aba1656b3d0d5cf842b6685
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-01-29 00:15:24 -0800

    Remove unnecessary files

--- chef-repo/site-cookbooks/image_magick/recipes/default.rb
+++ chef-repo/site-cookbooks/image_magick/recipes/default.rb
@@ -7,15 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+## Install Image Magick
+
 package "ImageMagick" do
         action :upgrade
         version "6.5.4.7"
 end
 
+
+## Install ImageMagick-devel
+
 package "ImageMagick-devel" do
         action :upgrade
         version "6.5.4.7"
 end
+
+
+## Install ipa-pgothic-fonts
 
 package "ipa-pgothic-fonts" do
         action :upgrade
--- chef-repo/site-cookbooks/mysql-server/templates/create_db.sql
+++
@@ -1,3 +0,0 @@
-create database redmine default character set utf8;
-grant all on redmine.* to redmine@localhost identified by 'redmine';
-flush privileges;
